cf49f3487102772827707ac89d38d5baffbb13e7
Fabien Taxil <fabien.taxil@gmail.com>
2022-08-27 22:35:35 +0100

    Added interactive python facilities

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
+# Configuration file for fish shell.
+#
+# Description:
+#
+#   fish is a smart and user-friendly command line shell for macOS, Linux, and
+#   the rest of the family. fish includes features like syntax highlighting,
+#   autosuggest-as-you-type, and fancy tab completions that just work, with no
+#   configuration required.
+#
+# Location:
+#
+#   ~/.config/fish/config.fish
+#
+
 # Remove the fish welcome message.
 set fish_greeting
 
@@ -5,4 +19,11 @@ set fish_greeting
 starship init fish | source
 
 # Add executable to PATH.
-fish_add_path ~/bin
+fish_add_path "~/bin"
+
+# Add some facility to the interactive python interpreter.
+set -x PYTHONSTARTUP "~/.pythonrc"
+
+# Define aliases.
+alias tree="tree -C"
+alias refresh="source ~/.config/fish/config.fish"
