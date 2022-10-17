32dcfc8406b716a00d0a8978ca4b4640d93a4410
Fabien Taxil <fabien.taxil@gmail.com>
2022-10-17 20:30:37 +0100

    Fixed lsp cmp deprecation

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,7 +4,7 @@ local function config(_config)
     return vim.tbl_deep_extend(
         "force",
         {
-            capabilities = require("cmp_nvim_lsp").update_capabilities(
+            capabilities = require("cmp_nvim_lsp").default_capabilities(
                 vim.lsp.protocol.make_client_capabilities()
             ),
             on_attach = function()
