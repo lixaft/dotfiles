9fe0d74a3355116a22e807519992029157dd0b2b
Fabien Taxil <lixaft@gmail.com>
2022-10-22 16:49:39 +0100

    Updated vim global folding maps

--- nvim/lua/lixaft/maps.lua
+++ nvim/lua/lixaft/maps.lua
@@ -1,28 +1,19 @@
-local Remap = require("lixaft.utilities.remap")
-local insert = Remap.insert
-local normal = Remap.normal
-local visual = Remap.visual
-
--- Move line around.
--- normal("<c-j>", ":move .+1<cr>==")
--- normal("<c-k>", ":move .-2<cr>==")
--- visual("<c-j>", ":move '>+1<cr>gv=gv")
--- visual("<c-k>", ":move '<-2<cr>gv=gv")
+local R = require("lixaft.utilities.remap")
 
 -- Easy configuration reloads.
-normal("<leader>ev", ":split $MYVIMRC<cr>")
-normal("<leader>sv", ":source $MYVIMRC<cr>")
+R.normal("<leader>ev", ":split $MYVIMRC<cr>")
+R.normal("<leader>sv", ":source $MYVIMRC<cr>")
 
 -- Make the current file executable.
-normal("<leader>x", "<cmd>!chmod +x %<cr>")
+R.normal("<leader>x", "<cmd>!chmod +x %<cr>")
 
 -- Global folding.
-normal("<leader>zO", "ggVGzO<c-o>")
-normal("<leader>zC", "ggVGzC<c-o>")
+R.normal("<leader>O", "ggVGzO<c-o>")
+R.normal("<leader>C", "ggVGzC<c-o>")
 
 -- Launch Netrw.
-normal("<leader>ps", ":Explore<cr>")
+R.normal("<leader>ps", ":Explore<cr>")
 
 -- Turn off highlighting.
-normal("<leader>h", ":noh<cr>")
-normal("<leader>s", ":set spell!<cr>")
+R.normal("<leader>h", ":noh<cr>")
+R.normal("<leader>s", ":set spell!<cr>")
