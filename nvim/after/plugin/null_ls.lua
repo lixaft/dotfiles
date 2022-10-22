367fabbefa7535eab42f9695041c15c38208c1b9
Fabien Taxil <lixaft@gmail.com>
2022-10-22 16:42:47 +0100

    Cleanup neovim linters

--- nvim/after/plugin/null_ls.lua
+++ nvim/after/plugin/null_ls.lua
@@ -3,7 +3,6 @@ require("null-ls").setup(
         sources = {
             require("null-ls").builtins.diagnostics.mypy,
             require("null-ls").builtins.diagnostics.flake8,
-            -- require("null-ls").builtins.diagnostics.pylint,
             require("null-ls").builtins.formatting.black,
             require("null-ls").builtins.formatting.isort,
         }
