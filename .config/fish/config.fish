4a64ed92e3e6f4c3192203583e3de4c15869a973
Fabien Taxil <lixaft@gmail.com>
2022-10-28 01:52:38 +0100

    Fixed and cleanup fish configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,36 +16,42 @@
 set fish_greeting ""
 
 # Define aliases.
-alias docker="podman"
 alias refresh="source $HOME/.config/fish/config.fish"
 alias tree="tree -C"
-alias vim="nvim"
-alias ls="exa"
-alias ll="exa --long --git --group-directories-first"
-alias la="exa --long --git --group-directories-first --all"
 
 # Extend $PATH.
 fish_add_path "$HOME/bin"
 
-# Make sure we pick Neovim as editor.
-set -x EDITOR "nvim"
-set -x VISUAL "nvim"
-
 # Python variables.
-set -x PIP_DISABLE_PIP_VERSION_CHECK "1"
-set -x PYTHONSTARTUP "$HOME/.pythonrc"
+set --export PIP_DISABLE_PIP_VERSION_CHECK "1"
+set --export PYTHONSTARTUP "$HOME/.pythonrc"
 
-# Initialize starship.
-if type -q "starship"
-    starship init fish | source
+# Replace `ls` by `exa` if available.
+if type --query "exa"
+    alias ls="exa"
+    alias ll="exa --long --git --group-directories-first"
+    alias la="ll --all"
 end
 
-# Source completions.
-if type -q "podman"
+# Pick neovim as editor <3
+if type --query "nvim"
+    alias vim="nvim"
+    set --export EDITOR "nvim"
+    set --export VISUAL "nvim"
+end
+
+# Use `podman` instead of `docker`.
+if type --query "podman"
+    alias docker="podman"
     podman completion fish | source
 end
 
-# Local configuration.
+# Initialize starship.
+if type --query "starship"
+    starship init fish | source
+end
+
+# Source local configuration.
 set local "$HOME/.config/fish/config.local.fish"
 if test -e "$local"
     source "$local"
