7b4b7d369d8d55a3c853a53e2df6e1eadf97c5d4
Fabien Taxil <lixaft@gmail.com>
2022-11-09 02:39:41 +0000

    Better neovim completion popup

--- src/.config/nvim/after/plugin/cmp.lua
+++ src/.config/nvim/after/plugin/cmp.lua
@@ -1,17 +1,52 @@
-require("cmp").setup(
-    {
-        sources = {
-            {name = "cmp_tabnine"},
-            {name = "nvim_lsp"},
+require("cmp").setup({
+    sources = {
+        { name = "nvim_lsp" },
+    },
+    window = {
+        completion = {
+            border ='rounded',
+            winhighlight ='Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None',
+            zindex = 1001,
+            scrolloff = 0,
+            col_offset = 0,
+            side_padding = 1,
         },
-        formatting = {
-            format = require("lspkind").cmp_format(
-                {
-                    mode = "symbol",
-                    maxwith = 50,
-                    ellipsis_char = "...",
-                }
-            )
-        },
-    }
-)
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            local icons = {
+                Text = "",
+                Method = "",
+                Function = "",
+                Constructor = "",
+                Field = "",
+                Variable = "",
+                Class = "ﴯ",
+                Interface = "",
+                Module = "",
+                Property = "ﰠ",
+                Unit = "",
+                Value = "",
+                Enum = "",
+                Keyword = "",
+                Snippet = "",
+                Color = "",
+                File = "",
+                Reference = "",
+                Folder = "",
+                EnumMember = "",
+                Constant = "",
+                Struct = "",
+                Event = "",
+                Operator = "",
+                TypeParameter = ""
+            }
+            vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+            local str = vim.split(vim_item.kind, "%s", { trimempty = true })
+            vim_item.kind = str[1] .. " "
+            vim_item.menu = "    (" .. str[2] .. ")"
+            return vim_item
+        end,
+    },
+})
--- src/.config/nvim/after/plugin/null_ls.lua
+++ src/.config/nvim/after/plugin/null_ls.lua
@@ -4,7 +4,11 @@ null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black.with({
+            extra_args = { "--line-length=79" },
+        }),
+        null_ls.builtins.formatting.isort.with({
+            extra_args = { "--force-single-line-imports" },
+        }),
     }
 })
