7c908208df2acfe0d8e06beae48513878cfe94d3
Fabien Taxil <lixaft@gmail.com>
2023-03-11 14:36:23 +0000

    fix(nvim): remove colored multi-lines todo comments

--- src/.config/nvim/lua/lixaft/plugins/todo_comments.lua
+++ src/.config/nvim/lua/lixaft/plugins/todo_comments.lua
@@ -6,5 +6,25 @@ return {
   "folke/todo-comments.nvim",
   lazy = true,
   event = { "BufReadPost", "BufNewFile" },
-  config = true,
+  opts = {
+    -- Highlighting of the line containing the todo comment.
+    -- * before: highlights before the keyword (typically comment characters)
+    -- * keyword: highlights of the keyword
+    -- * after: highlights after the keyword (todo text)
+    highlight = {
+      -- enable multine todo comments
+      multiline = true,
+
+      -- "fg" or "bg" or empty.
+      before = "",
+
+      -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty.
+      -- (wide and wide_bg is the same as bg, but will also highlight
+      -- surrounding characters, wide_fg acts accordingly but with fg).
+      keyword = "wide",
+
+      -- "fg" or "bg" or empty.
+      after = "",
+    },
+  },
 }
