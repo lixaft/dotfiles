dc41b82c4ebc392a251be841d4b2efa7ca544477
Fabien Taxil <lixaft@gmail.com>
2023-04-14 14:29:59 +0100

    feat(nvim): add yank highlight

--- nvim/.config/nvim/lua/lixaft/colors.lua
+++ nvim/.config/nvim/lua/lixaft/colors.lua
@@ -1,1 +1,9 @@
 vim.cmd([[colorscheme tokyonight]])
+
+-- Highlight yanked text.
+vim.cmd([[
+augroup highlight_yank
+  autocmd!
+   autocmd TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+augroup END
+]])
