4c27b854216b94712bfedc970dad4853bdf6571b
Fabien Taxil <lixaft@gmail.com>
2023-04-24 09:17:45 +0100

    refactor(nvim): convert yank highlihgt to lua

--- nvim/.config/nvim/lua/lixaft/colors.lua
+++ nvim/.config/nvim/lua/lixaft/colors.lua
@@ -1,9 +1,9 @@
+-- Set colorscheme.
 vim.cmd([[colorscheme tokyonight]])
 
 -- Highlight yanked text.
-vim.cmd([[
-augroup highlight_yank
-  autocmd!
-   autocmd TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
-augroup END
-]])
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 40 })
+  end,
+})
