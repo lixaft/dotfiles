7d00e8522e555f0622d37b36c95226992ea0793a
Fabien Taxil <lixaft@gmail.com>
2023-05-07 15:02:10 +0200

    feat(nvim): use <c-f> to accept copilot suggestion

--- nvim/.config/nvim/lua/lixaft/plugins/copilot.lua
+++ nvim/.config/nvim/lua/lixaft/plugins/copilot.lua
@@ -6,4 +6,13 @@ return {
   "github/copilot.vim",
   lazy = true,
   event = { "BufReadPre", "BufNewFile" },
+  init = function(_, _)
+    vim.api.nvim_set_keymap(
+      "i",
+      "<c-f>",
+      'copilot#Accept("<CR>")',
+      { silent = true, expr = true }
+    )
+    vim.g.copilot_no_tab_map = true
+  end,
 }
