981d9c542e4ee59952bf9fa9abe824a3500afd6b
Fabien Taxil <lixaft@gmail.com>
2023-05-28 11:41:12 +0200

    fix(nvim): restore autopep8 formatter

--- nvim/.config/nvim/lua/lixaft/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/lixaft/plugins/null-ls.lua
@@ -31,6 +31,7 @@ return {
         -- Python.
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.ruff,
 
         -- Cmake.
