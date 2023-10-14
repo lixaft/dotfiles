19e124ad8f134f72b354edcf7a73e188742c0f55
Fabien Taxil <lixaft@gmail.com>
2023-10-14 17:52:06 +0200

    feat(nvim): remove unused plugin

--- nvim/.config/nvim/lua/lixaft/plugins/surround.lua
+++
@@ -1,7 +0,0 @@
--- Delete/change/add parentheses/quotes/XML-tags/much more with ease.
-
-return {
-  "tpope/vim-surround",
-  event = { "BufNewFile", "BufReadPost" },
-  dependencies = { "tpope/vim-repeat" },
-}
