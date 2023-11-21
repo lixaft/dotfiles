2e2355ed295eb0a422c35ffd720d8248c49afeab
Fabien Taxil <lixaft@gmail.com>
2023-11-21 22:25:06 +0100

    fix(nvim): harpoon mapping conflicts

--- nvim/.config/nvim/lua/lixaft/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/lixaft/plugins/harpoon.lua
@@ -4,7 +4,7 @@ local partial = require("lixaft.utils.func").partial
 return {
   "ThePrimeagen/harpoon",
   keys = {
-    { "<leader>a", partial("harpoon.mark", "add_file") },
+    { "<leader>r", partial("harpoon.mark", "add_file") },
     { "<c-g>", partial("harpoon.ui", "toggle_quick_menu") },
     { "<c-h>", partial("harpoon.ui", "nav_file", { 1 }) },
     { "<c-t>", partial("harpoon.ui", "nav_file", { 2 }) },
