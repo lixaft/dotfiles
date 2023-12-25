c418ba580dc0bcb634af8abec3fb174931e66413
Fabien Taxil <lixaft@gmail.com>
2023-12-26 00:12:58 +0100

    feat(nvim): upgrade to harpoon2

--- nvim/.config/nvim/lua/lixaft/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/lixaft/plugins/harpoon.lua
@@ -1,14 +1,50 @@
 -- Getting you where you want with the fewest keystrokes.
-local partial = require("lixaft.utils.func").partial
+local map = require("lixaft.utils.keymap").set
 
 return {
   "ThePrimeagen/harpoon",
-  keys = {
-    { "<c-l>", partial("harpoon.mark", "add_file") },
-    { "<c-g>", partial("harpoon.ui", "toggle_quick_menu") },
-    { "<c-h>", partial("harpoon.ui", "nav_file", { 1 }) },
-    { "<c-t>", partial("harpoon.ui", "nav_file", { 2 }) },
-    { "<c-n>", partial("harpoon.ui", "nav_file", { 3 }) },
-    { "<c-s>", partial("harpoon.ui", "nav_file", { 4 }) },
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
   },
+
+  init = function()
+    local harpoon = require("harpoon")
+
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+      },
+    })
+
+    map("n", "<c-l>", function()
+      harpoon:list():append()
+    end)
+    map("n", "<c-g>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list(), {
+        border = "rounded",
+        title_pos = "center",
+      })
+    end)
+
+    map("n", "<c-h>", function()
+      harpoon:list():select(1)
+    end)
+    map("n", "<c-t>", function()
+      harpoon:list():select(2)
+    end)
+    map("n", "<c-n>", function()
+      harpoon:list():select(3)
+    end)
+    map("n", "<c-s>", function()
+      harpoon:list():select(4)
+    end)
+
+    map("n", "<leader>p", function()
+      harpoon:list():prev()
+    end)
+    map("n", "<leader>n", function()
+      harpoon:list():next()
+    end)
+  end,
 }
